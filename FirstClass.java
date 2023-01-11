2050f2283202107f3c781218c9aadbbdeb1ea3af
MeMrBerlin <nishanrkm@gmail.com>
2023-01-11 13:40:30 +0530

    java first class

---
+++ FirstClass.java
@@ -0,0 +1,10 @@
+class FirstClass
+{
+    public static void main(String args[])
+    {
+        int p =10, q = 20;
+        System.out.println("HELLO WORLD");
+        System.out.println(p+q);
+
+    }
+}
